b52984179fa0fe687b44236221299059c97b7384
Joe Moran <joemoran231@gmail.com>
2020-01-22 11:48:19 -0500

    Quick updates for admin links

--- src/components/admin/photos/PhotoThumbnail.vue
+++ src/components/admin/photos/PhotoThumbnail.vue
@@ -227,7 +227,7 @@ export default {
       );
       this.closePhotoEnlarge();
     },
-    deletePhoto: function() {
+    deletePhoto() {
       this.closeModal();
       let self = this;
       PhotoData.delete(this.selfPath, response => {
